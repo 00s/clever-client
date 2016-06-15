30d66492c907b1dbb3d3d396713958dc63482275
Pedro Rafael <pedrorafael0251@gmail.com>
2016-06-15 15:59:12 -0300

    Controller loginorcreate com fb_access_token

--- app.js
+++ app.js
@@ -3,20 +3,49 @@ var myApp = angular.module('clever', ['ezfb']);
 myApp.config(['ezfbProvider', function (ezfbProvider) {
   ezfbProvider
   .setInitParams({
-    appId: '737410249733152',
+    appId: '1521052684875334',
   });
 }]);
 
-myApp.controller('LoginCtrl', function(ezfb, $scope){
+myApp.controller('UserCtrl', function(ezfb, $scope, $http){
 	
-  $scope.login = function ($scope) {
-    ezfb.login(function (response)
-			   {scope: 'email'})
-			   .then(function (response) {
-					ezfb.api('/me', {fields: 'last_name'}, function(response) {
-  						console.log(response);
-					});
-				});
+  $scope.loginorcreate = function ($scope, $http) {
+    	ezfb.login(function (response){
+			if (response.authResponse != undefined){
+				localStorage.setItem("fb_access_token", response.authResponse.accessToken);
+				localStorage.setItem("fb_user_id", response.authResponse.userID);
+				localStorage.setItem("fb_expires_in", response.authResponse.expiresIn);
+			}else{
+				alert("Não é possível executar o login");
+			}
+		});
+	  
+	    if(window.localStorage.getItem("fb_access_token") != undefined){
+			var fb_access_token = localStorage.getItem("fb_access_token");
+			var string_fb_access_token = "AcessToken: " + fb_access_token;
+			console.log(string_fb_access_token);
+			//url = "http://cleverest.herokuapp.com/v1/loginorcreate";
+			//var cleverResponse = $http.post(url, {user_access_tokena: fb_access_token});
+			//console.log(cleverResponse);
+			
+		}else{
+			console.log("Usuário não logado!")
+		}
+	  
   };
-});
 	
+  $scope.loginorcreateteacher = function ($scope, $http) {
+    	ezfb.login(function (response){
+			if (response.authResponse != undefined){
+				localStorage.setItem("authResponse", response.authResponse);
+			}else{
+				alert("Não é possível executar o login");
+			}
+		});
+  };
+	
+	
+});
+			
+
+
